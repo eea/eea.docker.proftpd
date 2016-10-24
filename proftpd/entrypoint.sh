90b2b7badced9273a46648c73cd967eded7f880c
Mauro Michielon - EEA 06Oct2015 <michimau@galliwasp.eea.europa.eu>
2016-10-24 16:19:10 +0200

    First commit

---
+++ proftpd/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+sed "s#SERVERNAME#$SERVERNAME#g" -i /etc/proftpd/proftpd.conf
+sed "s#DEFAULTROOT#$DEFAULTROOT#g" -i /etc/proftpd/proftpd.conf
+sed "s#LDAPSERVER#$LDAPSERVER#g" -i /etc/proftpd/proftpd.conf
+
+sed "s#TLSCACERTIFICATEPATH#$TLSCACERTIFICATEPATH#g" -i /etc/proftpd/proftpd.conf
+sed "s#RSACERTIFICATEFILE#$RSACERTIFICATEFILE#g" -i /etc/proftpd/proftpd.conf
+sed "s#TLSCACERTIFICATEKEYFILE#$TLSCACERTIFICATEKEYFILE#g" -i /etc/proftpd/proftpd.conf
+
