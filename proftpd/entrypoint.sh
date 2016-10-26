b696f4508d080f6e0444eeef089f81eb874e9981
Mauro Michielon <mauro.michielon@eea.europa.eu>
2016-10-26 18:03:26 +0200

    Update entrypoint.sh

--- proftpd/entrypoint.sh
+++ proftpd/entrypoint.sh
@@ -5,6 +5,10 @@ sed "s#SERVERNAME#$SERVERNAME#g" -i /etc/proftpd/proftpd.conf
 sed "s#DEFAULTROOT#$DEFAULTROOT#g" -i /etc/proftpd/proftpd.conf
 sed "s#LDAPSERVER#$LDAPSERVER#g" -i /etc/proftpd/proftpd.conf
 
+sed "s#SERVERADMIN#$SERVERADMIN#g" -i /etc/proftpd/proftpd.conf
+sed "s#LDAPGENERATEHOMEDIRPREFIX#$LDAPGENERATEHOMEDIRPREFIX#g" -i /etc/proftpd/proftpd.conf
+sed "s#SERVERADMIN#$SERVERADMIN#g" -i /etc/proftpd/proftpd.conf
+
 sed "s#TLSCACERTIFICATEPATH#$TLSCACERTIFICATEPATH#g" -i /etc/proftpd/proftpd.conf
 sed "s#RSACERTIFICATEFILE#$RSACERTIFICATEFILE#g" -i /etc/proftpd/proftpd.conf
 sed "s#TLSCACERTIFICATEKEYFILE#$TLSCACERTIFICATEKEYFILE#g" -i /etc/proftpd/proftpd.conf
