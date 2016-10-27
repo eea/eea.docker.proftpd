0c21cec60d932154bf4d76ea7e2e08b993d5aa6a
Mauro Michielon <mauro.michielon@eea.europa.eu>
2016-10-27 18:08:04 +0200

    second commit

--- proftpd/entrypoint.sh
+++ proftpd/entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-sed "s#SERVERNAME#$SERVERNAME#g" -i /etc/proftpd/proftpd.conf
-sed "s#DEFAULTROOT#$DEFAULTROOT#g" -i /etc/proftpd/proftpd.conf
-sed "s#LDAPSERVER#$LDAPSERVER#g" -i /etc/proftpd/proftpd.conf
+sh /create_ssl_files.sh
 
-sed "s#SERVERADMIN#$SERVERADMIN#g" -i /etc/proftpd/proftpd.conf
-sed "s#LDAPGENERATEHOMEDIRPREFIX#$LDAPGENERATEHOMEDIRPREFIX#g" -i /etc/proftpd/proftpd.conf
-sed "s#SERVERADMIN#$SERVERADMIN#g" -i /etc/proftpd/proftpd.conf
+echo "$USERS" > /etc/proftpd/users
+chmod o-rwx /etc/proftpd/users
 
-sed "s#TLSCACERTIFICATEPATH#$TLSCACERTIFICATEPATH#g" -i /etc/proftpd/proftpd.conf
-sed "s#RSACERTIFICATEFILE#$RSACERTIFICATEFILE#g" -i /etc/proftpd/proftpd.conf
-sed "s#TLSCACERTIFICATEKEYFILE#$TLSCACERTIFICATEKEYFILE#g" -i /etc/proftpd/proftpd.conf
-
+exec proftpd -nc /etc/proftpd/proftpd.conf "$@"
